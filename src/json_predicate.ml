0cf62c88a5e588f2fdecd31a8bd108265aa5ca6b
Markus Weissmann <markus.weissmann@in.tum.de>
2015-12-02 15:27:49 +0100

    comply with new case-sensitivity definition

--- src/json_predicate.ml
+++ src/json_predicate.ml
@@ -1,13 +1,17 @@
+type case =
+  | Case_sensitive
+  | Case_insensitive
+ 
 let json_to_string = function
   | `String v -> v
   | x -> Yojson.Safe.to_string x
 
 let regexp = function
-  | true -> Str.regexp_case_fold
-  | false -> Str.regexp
+  | Case_insensitive -> Str.regexp_case_fold
+  | Case_sensitive -> Str.regexp
 
-let matches ignore_case needle =
-  let re = regexp ignore_case needle in
+let matches character_matching needle =
+  let re = regexp character_matching needle in
   (fun str ->
     try
       Str.string_match re str 0
@@ -15,9 +19,9 @@ let matches ignore_case needle =
     | Not_found -> false
   )
 
-let contains ignore_case needle =
+let contains character_matching needle =
   let s = Str.quote needle in
-  let re = regexp ignore_case s in
+  let re = regexp character_matching s in
   (fun str ->
     try
       let _ = Str.search_forward re str 0 in true
@@ -37,8 +41,8 @@ let predicate f =
 let p_string f value =
   predicate (fun x -> f (json_to_string value) (json_to_string x))
 
-let p_contains ignore_case =
-  p_string (contains ignore_case)
+let p_contains character_matching =
+  p_string (contains character_matching)
 
 let p_defined =
   predicate (fun _ -> true)
@@ -60,56 +64,56 @@ let p_in values =
 let p_less value =
   predicate (fun x -> value > x)
 
-let p_matches value ignore_case =
-  p_string (matches ignore_case) value
+let p_matches value character_matching =
+  p_string (matches character_matching) value
 
 let p_more value =
   predicate (fun x -> value < x)
 
-let p_starts ignore_case =
-  let starts ignore_case needle str =
+let p_starts character_matching =
+  let starts character_matching needle str =
     let needle_length = String.length needle in
     try
       let str = String.sub str 0 needle_length in
       let (str, needle) =
-        if ignore_case then (String.lowercase str, String.lowercase needle)
+        if character_matching = Case_insensitive then (String.lowercase str, String.lowercase needle)
         else (str, needle)
       in
       str = needle
     with
     | Invalid_argument _ -> false
   in
-  p_string (starts ignore_case)
+  p_string (starts character_matching)
 
-let p_ends ignore_case =
-  let ends ignore_case needle str =
+let p_ends character_matching =
+  let ends character_matching needle str =
     let needle_length = String.length needle in
     try
       let str = String.sub str (String.length str - needle_length) needle_length in
       let (str, needle) =
-        if ignore_case then (String.lowercase str, String.lowercase needle)
+        if character_matching = Case_insensitive then (String.lowercase str, String.lowercase needle)
         else (str, needle)
       in
       str = needle
     with 
     | Invalid_argument _ -> false
   in
-  p_string (ends ignore_case)
+  p_string (ends character_matching)
 
-let p_test ignore_case value =
+let p_test character_matching value =
   let ieq x y = String.lowercase x = String.lowercase y in
-  let rec eq_ignore_case (a : Yojson.Safe.json) (b : Yojson.Safe.json) =
+  let rec eq_character_matching (a : Yojson.Safe.json) (b : Yojson.Safe.json) =
     match (a, b) with
     | (x, y) when x = y -> true
-    | (`Assoc xs, `Assoc ys) -> List.for_all2 (fun (s1, x) (s2, y) -> (ieq s1 s2) && (eq_ignore_case x y)) xs ys
-    | (`List xs, `List ys) -> List.for_all2 eq_ignore_case xs ys
-    | (`Tuple xs, `Tuple ys) -> List.for_all2 eq_ignore_case xs ys
+    | (`Assoc xs, `Assoc ys) -> List.for_all2 (fun (s1, x) (s2, y) -> (ieq s1 s2) && (eq_character_matching x y)) xs ys
+    | (`List xs, `List ys) -> List.for_all2 eq_character_matching xs ys
+    | (`Tuple xs, `Tuple ys) -> List.for_all2 eq_character_matching xs ys
     | (`String x, `String y) -> ieq x y
     | _ -> false
   in
-  match ignore_case with
-  | false -> predicate (fun x -> x = value)
-  | true -> predicate (fun x -> try eq_ignore_case value x with | Not_found -> false)
+  match character_matching with
+  | Case_sensitive -> predicate (fun x -> x = value)
+  | Case_insensitive -> predicate (fun x -> try eq_character_matching value x with | Not_found -> false)
 
 let p_type value =
   let f =
@@ -148,15 +152,6 @@ let p_undefined p data =
   with
   | Not_found -> true
 
-let getbool (xs : (string * Yojson.Safe.json) list) (def : bool) (x : string) : bool =
-  try
-    begin match List.assoc x xs with
-    | `Bool v -> v
-    | x -> raise (Invalid_argument (Printf.sprintf "value '%s' not a boolean" (json_to_string x)))
-    end
-  with
-  | Not_found -> def
-
 let getstr (xs : (string * Yojson.Safe.json) list) x =
   match List.assoc x xs with
   | `String v -> v
@@ -170,19 +165,23 @@ let rec eval (pred : Yojson.Safe.json) =
   | `Assoc (xs : (string * Yojson.Safe.json) list) ->
     let get = fun x -> try List.assoc x xs with Not_found -> raise (Invalid_argument (Printf.sprintf "'%s' not found" x)) in
     let getstr = getstr xs in
-    let getbool = getbool xs false in
     let e () = raise (Invalid_argument "'path' not found") in
     let p def = try Json_pointer.of_string_exn (getstr "path") with Not_found -> (def ()) in
     begin match getstr "op" with
-    | "contains" -> p_contains (getbool "ignore_case") (get "value") (p e)
+    | "contains" -> p_contains Case_sensitive (get "value") (p e)
+    | "contains-" -> p_contains Case_insensitive (get "value") (p e)
     | "defined" -> p_defined (p e)
-    | "ends" -> p_ends (getbool "ignore_case") (get "value") (p e)
+    | "ends" -> p_ends Case_sensitive (get "value") (p e)
+    | "ends-" -> p_ends Case_insensitive (get "value") (p e)
     | "in" -> p_in (get "value") (p e)
     | "less" -> p_less (get "value") (p e)
-    | "matches" -> p_matches (get "value") (getbool "ignore_case") (p e)
+    | "matches" -> p_matches (get "value") Case_sensitive (p e)
+    | "matches-" -> p_matches (get "value") Case_insensitive (p e)
     | "more" -> p_more (get "value") (p e)
-    | "starts" -> p_starts (getbool "ignore_case") (get "value") (p e)
-    | "test" -> p_test (getbool "ignore_case") (get "value") (p e)
+    | "starts" -> p_starts Case_sensitive (get "value") (p e)
+    | "starts-" -> p_starts Case_insensitive (get "value") (p e)
+    | "test" -> p_test Case_sensitive (get "value") (p e)
+    | "test-" -> p_test Case_insensitive (get "value") (p e)
     | "type" -> p_type (get "value") (p e)
     | "undefined" -> p_undefined (p e)
     | "and" -> p_and (p (fun () -> Json_pointer.empty)) (get "apply")
